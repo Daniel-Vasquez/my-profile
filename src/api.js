2d4cf6f44409e9fa7ee9962f938934c465a426c2
Daniel Vasquez <dani.vasquez.dev@gmail.com>
2022-01-14 21:09:29 -0600

    Adding My Credit project

--- src/api.js
+++ src/api.js
@@ -25,42 +25,29 @@ const ProjectsData = [
   },
   {
     id: 2,
-    path: "platzi-video",
-    title: "Platzi video",
+    path: "mi-credito",
+    title: "Mi crédito",
     description:
-      "Website layout, usando solo HTML y CSS. Mi primer proyecto website, dónde refleja el amor por el CSS y el diseño, siguiendo la metodología de BEM.",
-    link: "https://daniel-vasquez.github.io/platzi-video/",
-    img: "platziVideo.png",
-    gif: "platziVideo.gif",
-    imageMobile: "mobilePlatziVideo.gif",
-    codigo: "https://github.com/Daniel-Vasquez/platzi-video",
+      "Simulador de crédito (landing page de una prueba técnica), con esta herramienta podrás simular financiamientos, para obtener cuánto terminarías pagando por un crédito.",
+    link: "https://mi-credito.vercel.app/",
+    img: "mi-credito-desktop.png",
+    gif: "mi-credito-GIF.gif",
+    imageMobile: "mi-credito-Mobile-GIF.gif",
+    codigo: "https://github.com/Daniel-Vasquez/mi-credito",
   },
   {
     id: 3,
-    path: "maquetacion",
-    title: "Maquetación",
-    description:
-      "Maquetación CSS y web components. Cuando recien comencé a aprender HTMl y CSS relicé este proyecto, pero en cada archivo de html copiaba y pegaba el código del header, así que tomé un curso de Web Components, dónde encapsulé todo el código del header en una etiqueta de html; que yo realicé en javaScript y así implementar mi etiqueta custom en cualquier archivo de html.",
-    link: "https://daniel-vasquez.github.io/maquetacion/",
-    img: "maquetacion.png",
-    gif: "maquetacion.gif",
-    imageMobile: "mobileMaquetacion.gif",
-    codigo: "https://github.com/Daniel-Vasquez/maquetacion",
+    path: "cachorros",
+    title: "Cachorros",
+    description: "Adopta uno y salva dos vidas. Nuestra fundación CachorrosOtraOportunidad reúne protectores de animales y personas que buscan adoptar una mascota.",
+    link: "https://cachorros.vercel.app/",
+    img: "cachorros.png",
+    gif: "cachorrosGif.gif",
+    imageMobile: "mobileCachorrosGif.gif",
+    codigo: "https://github.com/Daniel-Vasquez/cachorros",
   },
   {
     id: 4,
-    path: "responsive-design",
-    title: "Responsive design",
-    description:
-      "Mi primer boceto de 'Mi portafolio personal', diseñado con puro HTMl y CSS. Lo agregué a mis 'proyectos destacados' porque refleja la enorme diferencia entre el primer y el actual portafolio.",
-    link: "https://daniel-vasquez.github.io/responsive-design/",
-    img: "responsiveDesign.png",
-    gif: "responsiveDesign.gif",
-    imageMobile: "mobileResponsiDesign.gif",
-    codigo: "https://github.com/Daniel-Vasquez/responsive-design",
-  },
-  {
-    id: 6,
     path: "encontrar-numero",
     title: "Encuentra el número",
     description:
@@ -72,7 +59,43 @@ const ProjectsData = [
     codigo: "https://github.com/Daniel-Vasquez/encontrar-numero",
   },
   {
+    id: 5,
+    path: "platzi-video",
+    title: "Platzi video",
+    description:
+      "Website layout, usando solo HTML y CSS. Mi primer proyecto website, dónde refleja el amor por el CSS y el diseño, siguiendo la metodología de BEM.",
+    link: "https://daniel-vasquez.github.io/platzi-video/",
+    img: "platziVideo.png",
+    gif: "platziVideo.gif",
+    imageMobile: "mobilePlatziVideo.gif",
+    codigo: "https://github.com/Daniel-Vasquez/platzi-video",
+  },
+  {
+    id: 6,
+    path: "maquetacion",
+    title: "Maquetación",
+    description:
+      "Maquetación CSS y web components. Cuando recien comencé a aprender HTMl y CSS relicé este proyecto, pero en cada archivo de html copiaba y pegaba el código del header, así que tomé un curso de Web Components, dónde encapsulé todo el código del header en una etiqueta de html; que yo realicé en javaScript y así implementar mi etiqueta custom en cualquier archivo de html.",
+    link: "https://daniel-vasquez.github.io/maquetacion/",
+    img: "maquetacion.png",
+    gif: "maquetacion.gif",
+    imageMobile: "mobileMaquetacion.gif",
+    codigo: "https://github.com/Daniel-Vasquez/maquetacion",
+  },
+  {
     id: 7,
+    path: "responsive-design",
+    title: "Responsive design",
+    description:
+      "Mi primer boceto de 'Mi portafolio personal', diseñado con puro HTMl y CSS. Lo agregué a mis 'proyectos destacados' porque refleja la enorme diferencia entre el primer y el actual portafolio.",
+    link: "https://daniel-vasquez.github.io/responsive-design/",
+    img: "responsiveDesign.png",
+    gif: "responsiveDesign.gif",
+    imageMobile: "mobileResponsiDesign.gif",
+    codigo: "https://github.com/Daniel-Vasquez/responsive-design",
+  },
+  {
+    id: 8,
     path: "recordatorios",
     title: "Recordatorios",
     description:
@@ -82,18 +105,7 @@ const ProjectsData = [
     gif: "recordatorioGif.gif",
     imageMobile: "mobileRecordatorioGif.gif",
     codigo: "https://github.com/Daniel-Vasquez/reminder",
-  },
-  {
-    id: 8,
-    path: "cachorros",
-    title: "Cachorros",
-    description: "Adopta uno y salva dos vidas. Nuestra fundación CachorrosOtraOportunidad reúne protectores de animales y personas que buscan adoptar una mascota.",
-    link: "https://cachorros.vercel.app/",
-    img: "cachorros.png",
-    gif: "cachorrosGif.gif",
-    imageMobile: "mobileCachorrosGif.gif",
-    codigo: "https://github.com/Daniel-Vasquez/cachorros",
-  },
+  }
 ];
 
 export default ProjectsData;
